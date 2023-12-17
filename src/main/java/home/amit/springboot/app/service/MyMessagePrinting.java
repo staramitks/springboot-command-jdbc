ce06cbf938841d6aa56660745982e66231abe5b5
Amit Singh <star.amitks@gmail.com>
2023-12-17 13:27:42 +0530

    With Logs capturing results

---
+++ src/main/java/home/amit/springboot/app/service/MyMessagePrinting.java
@@ -0,0 +1,10 @@
+
+/*
+User :- AmitSingh
+Date :- 12/16/2023
+Time :- 10:59 PM
+Year :- 2023
+*/
+
+public class MyMessagePrinting {
+}
---
+++ src/test/java/home/amit/springboot/app/test/logging/MemoryAppender.java
@@ -0,0 +1,10 @@
+
+/*
+User :- AmitSingh
+Date :- 12/17/2023
+Time :- 12:04 PM
+Year :- 2023
+*/
+
+public class MemoryAppender {
+}
